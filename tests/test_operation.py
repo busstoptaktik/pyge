6f7bfaf619dc9eb3c6dc2301ac6beb83504c4aaf
Thomas Knudsen <knudsen.thomas@gmail.com>
2024-11-29 19:39:19 +0100

    Update tests for Operation

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,12 +1,17 @@
 from pyge.operation import Operation
-from pyge.context import Context
+from pytest import raises
+from pyge.minimal import MinimalContext
 
 
 def test_operation():
-    ctx = Context()
+    ctx = MinimalContext()
+
+    # Trying to access a non-existing OperationMethod raises NameError
+    with raises(NameError):
+        Operation("1234", "week_with_five_tuesdays", ctx)
 
     # Two proper steps and a lot of empty ones to be filtered out
-    op = Operation("1234", " ||||inv first a=1 b=2 | | | second c=3 d=4|| ", ctx)
+    op = Operation("1234", " ||||inv addone a=1 b=2 | | | subone c=3 d=4|| ", ctx)
 
     # Outer level
     assert op.args["_name"] == "pipeline"
@@ -17,8 +22,8 @@ def test_operation():
     # First step
     assert op.steps[0].id == "step"
     assert op.steps[0].inverted() is True
-    assert op.steps[0].args["_name"] == "first"
-    assert op.steps[0].definition == "inv first a=1 b=2"
+    assert op.steps[0].args["_name"] == "addone"
+    assert op.steps[0].definition == "inv addone a=1 b=2"
     assert op.steps[0].args["a"] == "1"
     assert op.steps[0].args["b"] == "2"
     assert op.steps[0].args["inv"] == ""
@@ -26,8 +31,8 @@ def test_operation():
     # Second step
     assert op.steps[1].id == "step"
     assert op.steps[1].inverted() is False
-    assert op.steps[1].args["_name"] == "second"
-    assert op.steps[1].definition == "second c=3 d=4"
+    assert op.steps[1].args["_name"] == "subone"
+    assert op.steps[1].definition == "subone c=3 d=4"
     assert op.steps[1].args["c"] == "3"
     assert op.steps[1].args["d"] == "4"
     assert "inv" not in op.steps[1].args
