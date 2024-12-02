5830abbf04714af4bcb47d8007c40ef9eb1b1c9c
Thomas Knudsen <knudsen.thomas@gmail.com>
2024-12-02 03:23:20 +0100

    Add the ellps cli entry point

--- src/cli/__init__.py
+++ src/cli/__init__.py
@@ -9,3 +9,12 @@ def plink():
     print("PLINK-PLONK!")
     plonk()
     agurk()
+
+def ellps():
+    from pyge.ellipsoid import Ellipsoid
+    e = Ellipsoid.named("GRS80")
+    print(f"Eccentricity [ppm]: {e.eccentricity()*1e6}")
+    print(f"Eccentricity squared [ppm]: {e.eccentricity_squared()*1e6}")
+    print(f"Third flattening [ppm]: {e.third_flattening()*1e6}")
+    print(f"Third flattening squared [ppm]: {(e.third_flattening()**2)*1e6}")
+    print(f"Eccentricity/Third flattening: {e.eccentricity()/e.third_flattening()}")
