f418dad0406e55513a2166fb3e54515f3ea5df4f
Thomas Knudsen <knudsen.thomas@gmail.com>
2024-12-04 12:55:52 +0100

    Abstract test implementation for CoordinateSet

--- tests/test_coordinateset.py
+++ tests/test_coordinateset.py
@@ -1,42 +1,72 @@
-from pyge.coordinateset import CoordinateSetColumnWise, CoordinateSetRowWise
+from pyge.coordinateset import (
+    CoordinateSet,
+    CoordinateSetColumnWise,
+    CoordinateSetRowWise,
+)
 from math import nan
 
+# Canonical dataset for testing of implementers of abstract base class
+# CoordinateSet
+coordinate_tuples = [
+    [11, 12, 13, 14],
+    [21, 22, 23, 24],
+    [31, 32, 33, 34],
+    [41, 42, 43, 44],
+    [51, 52, 53, 54],
+]
 
-def test_coordinatesetsoa():
-    x = [11, 12, 13, 14, 15]
-    y = [21, 22, 23, 24, 25]
-    z = [31, 32, 33, 34, 35]
-    t = [41, 42, 43, 44, 45]
-    soa = CoordinateSetColumnWise([x, y, z, t])
 
-    assert len(soa) == soa.len()
-    assert soa.len() == 5
-    assert soa.dim() == 4
+# Testing any object implementing the abstract base class "CoordinateSet",
+# under the assumption that its payload corresponds to the list-of-lists
+# coordinate_tuples above
+def abstract_test_coordinateset(coords: CoordinateSet):
+    assert coordinate_tuples[0] == [11, 12, 13, 14]
 
-    for i in range(len(soa)):
-        assert soa[i][0] in x
+    assert len(coords) == coords.len()
+    assert coords.len() == 5
+    assert coords.dim() == 4
 
-    assert soa.get(0) == [11, 21, 31, 41]
-    soa.set(0, [1, 2])
-    assert soa.get(0) == [1, 2, 31, 41]  # automatic passthru
+    for i in range(len(coords)):
+        assert coords[i][0] == coordinate_tuples[i][0]
+
+    assert coords.get(0) == coordinate_tuples[0]
+    coords.set(0, [1, 2])
+    assert coords.get(0) == [1, 2, 13, 14]  # automatic passthru
 
     # same set & get, but using indexed notation
-    assert soa[0] == [1, 2, 31, 41]
-    soa[0] = [11, 21]
-    assert soa[0] == [11, 21, 31, 41]
+    assert coords[0] == [1, 2, 13, 14]
+    coords[0] = [11, 12]
+    assert coords[0] == [11, 12, 13, 14]
 
     # Promote to 8 dimensions
     mask = [1, 2, 3, 4, 5, 6, 7, 8]
-    assert soa.promoted(0, mask) == [11, 21, 31, 41, 5, 6, 7, 8]
+    assert coords.promoted(0, mask) == [11, 12, 13, 14, 5, 6, 7, 8]
 
     # Default promotion (a no-op in this case)
-    assert soa.promoted(1) == [12, 22, 32, 42]
+    assert coords.promoted(1) == [21, 22, 23, 24]
 
-    # Mask overrides existing nan when default is provided and
+    # The mask overrides existing nan where a default is provided and
     # automatic passthru preserves untouched existing dimensions
     # when overwriting with lower dimensional arg
-    soa[1] = [nan]
-    assert soa.promoted(1, mask) == [1, 22, 32, 42, 5, 6, 7, 8]
+    coords[1] = [nan]
+    assert coords.promoted(1, mask) == [1, 22, 23, 24, 5, 6, 7, 8]
+
+    # Mask as Python tuple
+    coords[1] = [nan]
+    assert coords.promoted(1, (1, 2, 3, 4, 0, 2015)) == [1, 22, 23, 24, 0, 2015]
+
+
+# This test copies the material from coordinate_tuples, into coordinate_columns,
+# so changes does not affect the global values
+def test_coordinateset_column_wise():
+    coordinate_columns = [
+        [coordinate_tuples[r][c] for r in range(len(coordinate_tuples))]
+        for c in range(len(coordinate_tuples[0]))
+    ]
+    assert coordinate_columns[0] == [11, 21, 31, 41, 51]
+
+    coords = CoordinateSetColumnWise(coordinate_columns)
+    abstract_test_coordinateset(coords)
 
     # Default promotion in the 2D case
     x = [11, 12, 13, 14, 15]
@@ -44,52 +74,8 @@ def test_coordinatesetsoa():
     soa = CoordinateSetColumnWise([x, y])
     assert soa.promoted(1) == [12, 22, 0, nan]
 
-    # Mask as Python tuple
-    soa[1] = [nan]
-    assert soa.promoted(1, (1, 2, 0, 2015)) == [1, 22, 0, 2015]
 
-
-def test_coordinatesetaos():
-    x1 = [11, 12, 13, 14]
-    x2 = [21, 22, 23, 24]
-    x3 = [31, 32, 33, 34]
-    x4 = [41, 42, 43, 44]
-    x5 = [51, 52, 53, 54]
-    aos = CoordinateSetRowWise([x1, x2, x3, x4, x5])
-
-    assert len(aos) == aos.len()
-    assert aos.len() == 5
-    assert aos.dim() == 4
-
-    for i in range(len(aos)):
-        assert aos[i][0] in [11, 21, 31, 41, 51]
-
-    assert aos.get(0) == [11, 12, 13, 14]
-    aos.set(0, [1, 2])
-    assert aos.get(0) == [1, 2, 13, 14]  # automatic passthru
-
-    # same set & get, but using indexed notation
-    assert aos[0] == [1, 2, 13, 14]
-    aos[0] = [11, 21]
-    assert aos[0] == [11, 21, 13, 14]
-
-    # Promote to 8 dimensions
-    mask = [1, 2, 3, 4, 5, 6, 7, 8]
-    assert aos.promoted(0, mask) == [11, 21, 13, 14, 5, 6, 7, 8]
-
-    # Default promotion (a no-op in this case)
-    assert aos.promoted(1) == [21, 22, 23, 24]
-
-    # Mask overrides existing nan when default is provided
-    aos[1] = [nan]
-    assert aos.promoted(1, mask) == [1, 22, 23, 24, 5, 6, 7, 8]
-
-    # Default promotion in the 2D case
-    x = [11, 12, 13, 14, 15]
-    y = [21, 22, 23, 24, 25]
-    aos = CoordinateSetColumnWise([x, y])
-    assert aos.promoted(1) == [12, 22, 0, nan]
-
-    # Mask as Python tuple
-    aos[1] = [nan]
-    assert aos.promoted(1, (1, 2, 0, 2015)) == [1, 22, 0, 2015]
+# This test takes ownership of coordinate_tuples, so changes persist
+def test_coordinateset_row_wise():
+    coords = CoordinateSetRowWise(coordinate_tuples)
+    abstract_test_coordinateset(coords)
